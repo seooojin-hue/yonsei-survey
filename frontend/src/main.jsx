b133e70c3145d8ca7ab5cc579cdf60b44317233a
seooojin-hue <aikingemini@gmail.com>
2026-07-20 12:11:26 +0900

    add react router - split datahub and survey

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
 import SurveyPage from './SurveyPage.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SurveyPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/survey" element={<SurveyPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
