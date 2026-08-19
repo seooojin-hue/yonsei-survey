d6aa7137aac789401aeb10cd3d8bbfcc044a48bb
yiseo <aikingemini@gmail.com>
2026-08-19 19:35:29 +0900

    2주기 설문 문항 개정 및 URL 분리

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        {/* 전체 탭 관리자용 */}
         <Route path="/survey" element={<SurveyPage />} />
+        {/* 설문 대상별 개별 링크 */}
+        <Route path="/survey/student"           element={<SurveyPage surveyType="student" />} />
+        <Route path="/survey/industry"          element={<SurveyPage surveyType="industry" />} />
+        <Route path="/survey/graduate"          element={<SurveyPage surveyType="graduate" />} />
+        <Route path="/survey/awareness"         element={<SurveyPage surveyType="awareness" />} />
+        <Route path="/survey/exam-need"         element={<SurveyPage surveyType="exam-need" />} />
+        <Route path="/survey/exam-satisfaction" element={<SurveyPage surveyType="exam-satisfaction" />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
